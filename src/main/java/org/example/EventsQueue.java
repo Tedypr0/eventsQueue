579460352a9da61169e4690f93092e8a60ca9f46
tzhelyazkov <teodor.zhelyazkov99@gmail.com>
2023-01-26 13:00:33 +0200

    Removed custom Map. Changed normal Hashmap to a LinkedHashMap and synchronized it.

--- src/main/java/org/example/EventsQueue.java
+++ src/main/java/org/example/EventsQueue.java
@@ -1,206 +1,80 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map.Entry;
 
-// A node of chains
-class HashNode<K, V> {
-    K key;
-    V value;
-    final int hashCode;
-    HashNode<K, V> next;
-
-    public HashNode(K key, V value, int hashCode) {
-        this.key = key;
-        this.value = value;
-        this.hashCode = hashCode;
-    }
-}
-
-// Class to represent entire hash table
 class EventsQueue<T> {
-    private final Map<T, T> map;
-    private final java.util.Queue<T> queue;
+    private final ConcurrentHashMap<T, T> uniqueValuesMap;
+    private Map<T, T> queue = new LinkedHashMap<>();
     private int size;
     private final int capacity;
 
     public EventsQueue(int capacity) {
-        this.map = new Map<>();
-        queue = new LinkedBlockingQueue<>(capacity);
         this.capacity = capacity;
+        this.uniqueValuesMap = new ConcurrentHashMap<>();
+        queue = Collections.synchronizedMap(queue);
     }
 
     public synchronized void add(T element) throws InterruptedException {
         while (queue.size() == capacity) {
             wait();
         }
-        queue.add(element);
-        map.add(element, element);
+        uniqueValuesMap.put(element, element);
+        queue.put(element, element);
         size++;
         notifyAll();
     }
 
     public synchronized T poll() throws InterruptedException {
-        while (queue.isEmpty()) {
+        while (queue.size() == 0) {
             wait();
         }
-        T elementToRemove = queue.poll();
-        map.remove(elementToRemove);
-        size--;
-        notify();
-        return elementToRemove;
+        Entry<T, T> tempEntry = getFirstEntry();
+        if (tempEntry != null) {
+            T elementToRemove = queue.get(tempEntry.getKey());
+            queue.remove(tempEntry.getKey());
+            if (!queue.containsKey(tempEntry.getKey())) {
+                uniqueValuesMap.remove(tempEntry.getKey());
+            }
+            size--;
+            notify();
+            return elementToRemove;
+        }
+        return null;
     }
 
     public synchronized T peek() throws InterruptedException {
         while (queue.isEmpty()) {
             wait();
         }
-        return queue.peek();
-    }
-    public int size() {
-        return size;
-    }
-}
-
-class Map<K, V> {
-    // bucketArray is used to store array of chains
-    private List<HashNode<K, V>> bucketArray;
-
-    // Current capacity of array list
-    private int numBuckets;
-
-    // Current size of array list
-    private int size;
-
-    public Map() {
-        bucketArray = new ArrayList<>();
-        numBuckets = 10;
-        size = 0;
-
-        // Create empty chains
-        for (int i = 0; i < numBuckets; i++)
-            bucketArray.add(null);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size() == 0;
-    }
-
-    private int hashCode(K key) {
-        return Objects.hashCode(key);
-    }
-
-    // This implements hash function to find index
-    // for a key
-    private synchronized int getBucketIndex(K key) {
-        int hashCode = hashCode(key);
-        int index = hashCode % numBuckets;
-        // key.hashCode() could be negative.
-        index = index < 0 ? index * -1 : index;
-        return index;
-    }
-
-    // Method to remove a given key
-    public synchronized V remove(K key) {
-        // Apply hash function to find index for given key
-        int bucketIndex = getBucketIndex(key);
-        int hashCode = hashCode(key);
-        // Get head of chain
-        HashNode<K, V> head = bucketArray.get(bucketIndex);
-
-        // Search for key in its chain
-        HashNode<K, V> prev = null;
-        while (head != null) {
-            // If Key found
-            if (head.key.equals(key) && hashCode == head.hashCode)
-                break;
-
-            // Else keep moving in chain
-            prev = head;
-            head = head.next;
+        Entry<T, T> entry = getFirstEntry();
+        if (entry != null) {
+            return queue.get(entry.getKey());
         }
-
-        // If key was not there
-        if (head == null)
-            return null;
-
-        // Reduce size
-        size--;
-
-        // Remove key
-        if (prev != null)
-            prev.next = head.next;
-        else
-            bucketArray.set(bucketIndex, head.next);
-
-        return head.value;
-    }
-
-    // Returns value for a key
-    public synchronized V get(K key) {
-        // Find head of chain for given key
-        int bucketIndex = getBucketIndex(key);
-        int hashCode = hashCode(key);
-
-        HashNode<K, V> head = bucketArray.get(bucketIndex);
-
-        // Search key in chain
-        while (head != null) {
-            if (head.key.equals(key) && head.hashCode == hashCode)
-                return head.value;
-            head = head.next;
-        }
-
-        // If key not found
         return null;
     }
 
-    // Adds a key value pair to hash
-    public synchronized void add(K key, V value) {
-        // Find head of chain for given key
-        int bucketIndex = getBucketIndex(key);
-        int hashCode = hashCode(key);
-        HashNode<K, V> head = bucketArray.get(bucketIndex);
-        HashNode<K, V> newNode = new HashNode<>(key, value, hashCode);
-        // Check if key is already present
-        while (head != null) {
-            if (head.key.equals(key) && head.hashCode == hashCode && head.next == null) {
-                head.next = newNode;
-                size++;
-                return;
-            }
-            head = head.next;
+    public int size() {
+        return size;
+    }
+
+    public synchronized Collection<T> getUniqueValues() {
+        return uniqueValuesMap.values();
+    }
+
+    private Entry<T, T> getFirstEntry() {
+        for (Entry<T, T> entry : queue.entrySet()) {
+            return entry;
         }
+        return null;
+    }
 
-        // Insert key in chain
-        size++;
-        head = bucketArray.get(bucketIndex);
-
-        newNode.next = head;
-        bucketArray.set(bucketIndex, newNode);
-
-        // If load factor goes beyond threshold, then
-        // double hash table size
-        if ((1.0 * size) / numBuckets >= 0.7) {
-            List<HashNode<K, V>> temp = bucketArray;
-            bucketArray = new ArrayList<>();
-            numBuckets = 2 * numBuckets;
-            size = 0;
-            for (int i = 0; i < numBuckets; i++)
-                bucketArray.add(null);
-
-            for (HashNode<K, V> headNode : temp) {
-                while (headNode != null) {
-                    add(headNode.key, headNode.value);
-                    headNode = headNode.next;
-                }
-            }
+    public synchronized T removeSpecificElement(T elementToRemove) {
+        queue.remove(elementToRemove);
+        if (!queue.containsKey(elementToRemove)) {
+            uniqueValuesMap.remove(elementToRemove);
         }
+        return elementToRemove;
     }
 }
