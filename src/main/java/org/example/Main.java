6df332de098188f6dc3c2f2342c8786b13e907fd
tzhelyazkov <teodor.zhelyazkov99@gmail.com>
2023-02-15 16:16:05 +0200

    Add Event class and Main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
 public class Main {
-
-    public static void main(String[] args) throws InterruptedException {
-        SideStuff stuff = new SideStuff();
-        stuff.eventReferencesCreationAndThreadStartup();
-        stuff.eventCreation();
+    public static void main(String[] args) {
+        Helper startup = new Helper();
     }
 }
