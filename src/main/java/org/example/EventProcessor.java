21661a5ac638cd1e0c1ff64b461bf16430786315
tzhelyazkov <teodor.zhelyazkov99@gmail.com>
2023-02-15 16:15:39 +0200

    Solution with a normal Queue.

---
+++ src/main/java/org/example/EventProcessor.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class EventProcessor extends Thread {
+    private final AtomicBoolean isPoisonFound;
+    private final UniqueEventsQueue<Event> queue;
+
+    public EventProcessor(AtomicBoolean isPoisonFound, UniqueEventsQueue<Event> queue) {
+        this.isPoisonFound = isPoisonFound;
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        Event event;
+        while (!isPoisonFound.get()) {
+            try {
+                event = Helper.peekPoll(queue, this);
+                print(event);
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException();
+            }
+        }
+    }
+
+    private synchronized void print(Event event) {
+        if (event != null) {
+            System.out.printf("Thread %s successfully finished %s with key %d%n", this.getName(), event.getMessage(), event.getId());
+        }
+    }
+
+}
---
+++ src/main/java/org/example/UniqueEventsQueue.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class UniqueEventsQueue<T> {
+    private final Queue<T> queue;
+    private int size;
+
+    public UniqueEventsQueue() {
+        queue = new ConcurrentLinkedQueue<>();
+    }
+
+    public synchronized void add(T element) {
+        queue.add(element);
+        size++;
+        notifyAll();
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait();
+        }
+        size--;
+        notify();
+        return queue.poll();
+    }
+
+    public synchronized T peek() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait();
+        }
+        return queue.peek();
+    }
+}
